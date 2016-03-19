73eebc1e46e93f8645d899c532ba656909a80815
Konrad Stobiecki <konrad@maximus.net.pl>
2016-03-19 01:44:41 +0100

    added compatibility for isolated styles

--- relativityjs.js
+++ relativityjs.js
@@ -7,6 +7,8 @@
     function RelativityJs () {};
     RelativityJs.prototype = {
         methodName: (function () {
+            console.log(document.currentScript);
+            console.log(document._currentScript);
             return (document._currentScript === undefined) ? 'currentScript' : '_currentScript';
         })(),
         template: function (func) {
@@ -16,34 +18,38 @@
             for(var i = 0; i < templates.length; i++) {
                 //create element
                 var template = templates[i];
+
+                template.innerHTML = template.innerHTML.replace('<style>','<style scoped>');
                 var el = Object.create(HTMLElement.prototype);
 
                 //create callback
-                el.createdCallback = function() {
-                    //replace content like in shadow element
-                    template.content.querySelector('content').innerHTML = this.innerHTML;
-                    this.innerHTML = "";
-
-                    // import template
-                    var clone = document.importNode(template.content, true);
-
-                    func(this, clone);
-                };
+                this['template'+func](el, template);
 
                 //register element
                 document.registerElement(template.id, {prototype: el});
             }
         },
-        templateInherit: function() {
-            this.template(function (ref, clone) {
-                ref.appendChild(clone);
-            });
+        templateInherit: function(el, template) {
+            el.createdCallback = function () {
+                //replace content like in shadow element
+                template.content.querySelector('content').innerHTML = this.innerHTML;
+                this.innerHTML = "";
+
+
+                //create dom node
+                var domNode = document.importNode(template.content, true);
+                //create root element
+                this.appendChild(domNode);
+            };
+
         },
-        templateIsolate: function () {
-            this.template(function (ref, clone) {
-                ref.createShadowRoot().appendChild(clone);
-            });
+        templateIsolate: function (el, template) {
+            el.createdCallback = function () {
+                //create dom node
+                var domNode = document.importNode(template.content, true);
+                this.createShadowRoot().appendChild(domNode);
+            };
         }
-    }
+    };
     window.relativityJs = new RelativityJs();
 })();
